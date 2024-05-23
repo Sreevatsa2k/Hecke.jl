7388b6a2596c8a4ac5b7bc7d762860597080dd67
Sreevatsa Sasisekar <sasiseka@mathematik.uni-kl.de>
2024-05-23 13:15:42 +0200

    Removed String comparisons

--- test/Misc/Integer.jl
+++ test/Misc/Integer.jl
@@ -35,21 +35,12 @@
 
     @test Hecke.ecm(ZZ(2)) == (0,2)
 
-    @test string(FacElem(Hecke.factor(ZZ(2)))) == "2^1"
-
     @test carmichael_lambda(Hecke.factor(ZZ(2))) == 1
-
-    @test string(factor(FacElem(factor(ZZ(2))))) == "1 * 2"
-
-    @test string(Divisors(factor(ZZ(2)))) == "Divisors of 2 = MSet(2)"
 
     @test euler_phi(factor(FacElem(factor(ZZ(2))))) == 1
 
     @test carmichael_lambda(factor(FacElem(factor(ZZ(2))))) == 1
 
-    @test string(sunit_group_fac_elem([2])) == "(Z/2 x Z, SUnits (in factored form) map of Factored elements over Rational field for ZZRingElem[-1, 2]\n)"
-
-    
     
 end
 
