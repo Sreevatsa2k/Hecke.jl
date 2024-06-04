8c48955e8d57969f0f823012863e652ea3721262
Sreevatsa Sasisekar <sasiseka@mathematik.uni-kl.de>
2024-06-04 20:23:20 +0200

    Added Tests

--- test/Misc/coprime.jl
+++ test/Misc/coprime.jl
@@ -12,6 +12,11 @@
 
 @test Hecke.copy_into!(QQ(2), QQ(3)) == 3
 
+Qx, x = polynomial_ring(QQ, "x");
+K, a = number_field(x^2 + 1, "a");
+L, b = number_field(x^2 - 2, "b");
+@test Hecke.copy_into!(minpoly(a), minpoly(b)) == x^2 - 2
+
 @test Hecke.ppgle(ZZ(100), ZZ(200)) == (100, 1, 100)
 
 @test Hecke.pair_bernstein(ZZ(1000000), ZZ(2000000)) == [15625, 1, 1, 1, 1, 2]
@@ -29,6 +34,8 @@
 @test Hecke.augment_bernstein([ZZ(1000000), ZZ(2000000)], ZZ(2)) == [15625, 2, 15625, 2]
 
 @test sort(Hecke.split_bernstein(ZZ(2), [ZZ(1000000), ZZ(2000000)])) == [(1000000, 2), (2000000, 2)]
+
+@test Hecke.split_bernstein(ZZ(20000), [ZZ(20000), ZZ(30000)]) == [(ZZ(30000), ZZ(20000)), (ZZ(20000), ZZ(20000))]
 
 @test Hecke.augment_bach([ZZ(20), ZZ(30)], ZZ(2)) == [5, 15, 2]
 
