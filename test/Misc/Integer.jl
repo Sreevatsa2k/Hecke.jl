2446220fdd450177f3e9022130da90011d1ccb29
Sreevatsa Sasisekar <sasiseka@mathematik.uni-kl.de>
2024-06-11 18:20:51 +0200

    Tests for integer.jl

--- test/Misc/Integer.jl
+++ test/Misc/Integer.jl
@@ -13,7 +13,9 @@
 
     @test Hecke.is_prime_power(8)
 
-    @test sort(euler_phi_inv(2)) == [3,4,6]
+    @test sort(euler_phi_inv(4)) == [5,8,10,12]
+
+    @test sort(euler_phi_inv(200)) == [275,303,375,404,500,550,606,750]
 
     @test euler_phi(ZZ(5)) == ZZ(4)
 
@@ -25,13 +27,17 @@
 
     @test carmichael_lambda(80) == 4
 
-    @test Hecke.squarefree_up_to(20) == [1, 2, 3, 5, 6, 7, 10, 11, 13, 14, 15, 17, 19]
+    @test Hecke.squarefree_up_to(21) == [1, 2, 3, 5, 6, 7, 10, 11, 13, 14, 15, 17, 19, 21]
 
     @test support(QQ(2)) == [2]
+
+    @test sort(support(QQ(200//3))) == [2, 3, 5]
 
     @test sort(euler_phi_inv(2)) == ZZ.([3,4,6])
 
     @test radical(200) == ZZ(10)
+
+    @test collect(factor(ZZ, QQ(200//3))) == [5 => 2, 2 => 3, 3 => -1]
 
     @test Hecke.ecm(ZZ(2)) == (0,2)
 
@@ -72,12 +78,25 @@
 
     @test sort(collect(Divisors(factor(ZZ(20))))) == ZZ.([1, 2, 4, 5, 10, 20])
 
-    Qx, x = polynomial_ring(FlintQQ, "x");
+    Qx, x = polynomial_ring(QQ, "x");
     K, a = number_field(x^2 + 1, "a");
     A = ideal(EquationOrder(K), ZZ(23));
     C = ideal(EquationOrder(K), ZZ(1));
     @test collect(Divisors(A)) == [C, A]
-
     
+    Qx, x = polynomial_ring(QQ, "x");
+    K, a = number_field(x^2 + 1, "a");
+    A = ideal(EquationOrder(K), ZZ(23));
+    C = ideal(EquationOrder(K), ZZ(1));
+    factorization = factor(A);
+    Dict1 = Dict{AbsNumFieldOrderIdeal{AbsSimpleNumField, AbsSimpleNumFieldElem}, ZZRingElem}();
+    for (factor, exp) in factorization
+        Dict1[factor] = exp
+    end
+    converted_A = sum([factor^exp for (factor, exp) in factorization]);
+    result = collect(Divisors(converted_A));
+    expected = [C,A];
+    @test expected == result
+
 
 end
